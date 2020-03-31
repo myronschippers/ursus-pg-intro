fbf54e8f9721b0083d4a6f3f951fceed51e35765
Myron <dubtwee@gmail.com>
2020-03-31 11:41:30 -0500

    creating songs get route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require('pg');
 
 const app = express();
+const Pool = pg.Pool;
 const PORT = process.env.PORT || 5000;
+
+const pool = new Pool({
+  database: 'music_storage',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMills: 30000,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +22,16 @@ app.use(express.static('public'));
 
 // GET ROUTE for All Songs
 app.get('/songs', (req, res) => {
-  
+  const queryText = `SELECT * FROM "songs";`;
+  pool.query(queryText)
+    .then((responseDB) => {
+      const dbRows = responseDB.rows;
+      res.send(dbRows);
+    })
+    .catch((err) => {
+      console.log('ERROR:', err);
+      res.sendStatus(500);
+    });
 });
 
 // POST ROUTE for saving a song
